11982e8b25c535c002772e460ccf4afbd0587d43
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-08-11 18:44:52 -0300

    Importando componente.

--- fundamentos/fundamentos/src/App.js
+++ fundamentos/fundamentos/src/App.js
@@ -1,9 +1,14 @@
+// components
+import FirstComponent from "./components/FirstComponent";
+
+// styles / CSS
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos</h1>
+      <FirstComponent />
     </div>
   );
 }
