3cd5213fec67c055a5d2b0b8f9930de4f3542f92
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-10-01 18:51:06 -0300

    Trabalhando com eventos.

---
+++ fundamentos/fundamentos/src/components/Events.js
@@ -0,0 +1,16 @@
+const Events = () => {
+  const handleMyEvent = (e) => {
+    console.log(e);
+    console.log("Ativou o evento!");
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleMyEvent}>Clique aqui!</button>
+      </div>
+    </div>
+  );
+};
+
+export default Events;
