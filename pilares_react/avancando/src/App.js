251b6c890ffa2ae79a7018a5050cbb8c55826dad
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-10-23 17:58:48 -0300

    Adiciona imagem a partir do /assets.

--- pilares_react/avancando/src/App.js
+++ pilares_react/avancando/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+import City from "./assets/city.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React</h1>
       <div>
-        <img src="/img1.jpg" alt="Borboleta" />
-        {/* <img src={city} alt="Cidade" /> */}
+        <img src="/img1.jpg" alt="Borboleta" height={"300px"} />
+        <img src={City} alt="Cidade" height={"300px"} />
       </div>
     </div>
   );
