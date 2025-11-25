a5b19ffd258da766b7c1102944388add741b953f
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-11-25 17:41:39 -0300

    Alterando e rerenderizando valores de variáveis com o hook useState.

--- pilares_react/avancando/src/App.js
+++ pilares_react/avancando/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import City from "./assets/city.jpg";
+import ManageData from "./components/ManageData";
 
 function App() {
   return (
@@ -7,8 +8,11 @@ function App() {
       <h1>Avançando em React</h1>
       <div>
         <img src="/img1.jpg" alt="Borboleta" height={"300px"} />
+      </div>
+      <div>
         <img src={City} alt="Cidade" height={"300px"} />
       </div>
+      <ManageData />
     </div>
   );
 }
