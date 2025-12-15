6141fe0c8b231089a7393bbd39d1355daabbac18
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-12-15 18:43:04 -0300

    Utilizando chave única na propriedade 'key'.

--- pilares_react/avancando/src/components/ListRender.js
+++ pilares_react/avancando/src/components/ListRender.js
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 const ListRender = () => {
   const [list] = useState(["Matheus", "Pedro", "Josias"]);
+  const [users, setUsers] = useState([
+    { id: 1, name: "Matheus", age: 31 },
+    { id: 2, name: "Jones", age: 19 },
+    { id: 3, name: "Scorpion", age: 201 },
+  ]);
 
   return (
     <div>
       <ul>
         {list.map((item, i) => (
           <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
         ))}
       </ul>
     </div>
